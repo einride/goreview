2260ae03586b45ad23a52a479cac560483d05615
Magnus Röös <magnus.roos@einride.tech>
2020-01-13 19:53:28 +0100

    backward compability on error message formatting

--- internal/passes/pkgerrors/analyzer.go
+++ internal/passes/pkgerrors/analyzer.go
@@ -1,6 +1,13 @@
 package pkgerrors
 
-import "golang.org/x/tools/go/analysis"
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"golang.org/x/tools/go/analysis"
+)
 
 const Doc = `check for usage of deprecated github.com/pkg/errors library`
 
@@ -8,6 +15,7 @@ const (
 	pkgErrorsImportPath = `"github.com/pkg/errors"`
 	xErrorsImportPath   = `"golang.org/x/xerrors"`
 	errorMessage        = "use " + pkgErrorsImportPath + " instead of " + xErrorsImportPath
+	errorMessageOld     = "use " + xErrorsImportPath + " instead of " + pkgErrorsImportPath
 )
 
 func Analyzer() *analysis.Analyzer {
@@ -19,10 +27,25 @@ func Analyzer() *analysis.Analyzer {
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	version := strings.Split(runtime.Version(), ".")
+	if version[0] != "go1" {
+		return nil, fmt.Errorf("unsupported major version: %s", version[0])
+	}
+	minor, err := strconv.Atoi(version[1])
+	if err != nil {
+		return nil, fmt.Errorf("unsupported minor version: %s", version[1])
+	}
+
 	for _, f := range pass.Files {
 		for _, importSpec := range f.Imports {
-			if importSpec.Path.Value == xErrorsImportPath {
-				pass.Reportf(importSpec.Pos(), errorMessage)
+			if minor >= 13 {
+				if importSpec.Path.Value == xErrorsImportPath {
+					pass.Reportf(importSpec.Pos(), errorMessage)
+				}
+			} else {
+				if importSpec.Path.Value == pkgErrorsImportPath {
+					pass.Reportf(importSpec.Pos(), errorMessageOld)
+				}
 			}
 		}
 	}
