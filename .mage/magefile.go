fda5e31bf49c5d21d76922f6ac74516ddf6ff0b5
Oscar Söderlund <odsod@users.noreply.github.com>
2022-01-06 18:27:27 +0100

    ci: migrate to mage-tools

---
+++ .mage/magefile.go
@@ -0,0 +1,55 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+	"go.einride.tech/mage-tools/mglog"
+	"go.einride.tech/mage-tools/mgmake"
+	"go.einride.tech/mage-tools/mgpath"
+
+	// mage:import
+	"go.einride.tech/mage-tools/targets/mgconvco"
+
+	// mage:import
+	"go.einride.tech/mage-tools/targets/mggo"
+
+	// mage:import
+	"go.einride.tech/mage-tools/targets/mggolangcilint"
+
+	// mage:import
+	"go.einride.tech/mage-tools/targets/mgmarkdownfmt"
+
+	// mage:import
+	"go.einride.tech/mage-tools/targets/mggitverifynodiff"
+)
+
+func init() {
+	mgmake.GenerateMakefiles(
+		mgmake.Makefile{
+			Path:          mgpath.FromGitRoot("Makefile"),
+			DefaultTarget: All,
+		},
+	)
+}
+
+func All() {
+	mg.Deps(
+		mg.F(mgconvco.ConvcoCheck, "origin/master..HEAD"),
+		mggolangcilint.GolangciLint,
+		mggo.GoTest,
+		Goreview,
+		mgmarkdownfmt.FormatMarkdown,
+	)
+	mg.SerialDeps(
+		mggo.GoModTidy,
+		mggitverifynodiff.GitVerifyNoDiff,
+	)
+}
+
+func Goreview() error {
+	mglog.Logger("goreview").Info("running...")
+	return sh.RunV("go", "run", ".", "-c", "1", "./...")
+}
