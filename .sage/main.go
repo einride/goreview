24c27ca7ee30cb1f626c74df2b8eb9ae7bd350c1
Oscar Söderlund <odsod@users.noreply.github.com>
2023-07-26 16:38:04 +0200

    feat(deps): bump deps

--- .sage/main.go
+++ .sage/main.go
@@ -8,6 +8,8 @@ import (
 	"go.einride.tech/sage/tools/sggit"
 	"go.einride.tech/sage/tools/sggo"
 	"go.einride.tech/sage/tools/sggolangcilint"
+	"go.einride.tech/sage/tools/sggoreleaser"
+	"go.einride.tech/sage/tools/sggosemanticrelease"
 	"go.einride.tech/sage/tools/sgmarkdownfmt"
 	"go.einride.tech/sage/tools/sgyamlfmt"
 )
@@ -16,12 +18,12 @@ func main() {
 	sg.GenerateMakefiles(
 		sg.Makefile{
 			Path:          sg.FromGitRoot("Makefile"),
-			DefaultTarget: All,
+			DefaultTarget: Default,
 		},
 	)
 }
 
-func All(ctx context.Context) error {
+func Default(ctx context.Context) error {
 	sg.Deps(ctx, ConvcoCheck, GoLint, GoReview, GoTest, FormatMarkdown, FormatYAML)
 	sg.SerialDeps(ctx, GoModTidy, GitVerifyNoDiff)
 	return nil
@@ -65,4 +67,38 @@ func ConvcoCheck(ctx context.Context) error {
 func GitVerifyNoDiff(ctx context.Context) error {
 	sg.Logger(ctx).Println("verifying that git has no diff...")
 	return sggit.VerifyNoDiff(ctx)
+}
+
+func SemanticRelease(ctx context.Context, repo string, dry bool) error {
+	sg.Logger(ctx).Println("triggering release...")
+	args := []string{
+		"--allow-initial-development-versions",
+		"--allow-no-changes",
+		"--ci-condition=default",
+		"--provider=github",
+		"--provider-opt=slug=" + repo,
+	}
+	if dry {
+		args = append(args, "--dry")
+	}
+	return sggosemanticrelease.Command(ctx, args...).Run()
+}
+
+func GoReleaser(ctx context.Context, snapshot bool) error {
+	sg.Logger(ctx).Println("building Go binary releases...")
+	if err := sggit.Command(ctx, "fetch", "--force", "--tags").Run(); err != nil {
+		return err
+	}
+	args := []string{
+		"release",
+		"--clean",
+	}
+	if len(sggit.Tags(ctx)) == 0 && !snapshot {
+		sg.Logger(ctx).Printf("no git tag found for %s, forcing snapshot mode", sggit.ShortSHA(ctx))
+		snapshot = true
+	}
+	if snapshot {
+		args = append(args, "--snapshot")
+	}
+	return sggoreleaser.Command(ctx, args...).Run()
 }
